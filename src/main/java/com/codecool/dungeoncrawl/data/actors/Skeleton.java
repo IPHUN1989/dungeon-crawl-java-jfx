6aca405ddc00464655f224dd52d4b6c97b146714
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-06-21 14:23:20 +0200

    implement move behaviour for skeleton

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Skeleton.java
@@ -2,8 +2,8 @@ package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
 
-public class Skeleton extends Actor implements MonsterMove{
-
+public class Skeleton extends Actor implements MonsterMove {
+    private int direction = -1;
 
     public Skeleton(Cell cell) {
         super(cell);
@@ -11,8 +11,20 @@ public class Skeleton extends Actor implements MonsterMove{
 
     @Override
     public void move() {
-
+        Cell nextLeftCell = getCell().getNeighbor(-1, 0);
+        Cell nextRightCell = getCell().getNeighbor(1,0);
+        if(!nextLeftCell.getType().isWalkable() || !nextRightCell.getType().isWalkable()){
+            changeDirection();
+            generalMove(direction,0);
+        } else {
+            generalMove(direction,0);
+        }
     }
+
+    private void changeDirection () {
+        this.direction = this.direction * -1;
+    }
+
 
     @Override
     public String getTileName() {
