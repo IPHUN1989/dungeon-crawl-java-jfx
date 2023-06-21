9f40486926f1ae44149e341a805da92e4bbc18d3
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-06-21 14:28:07 +0200

    implement move behaviour for dragon

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Dragon.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Dragon.java
@@ -3,6 +3,7 @@ package com.codecool.dungeoncrawl.data.actors;
 import com.codecool.dungeoncrawl.data.Cell;
 
 public class Dragon extends Actor implements MonsterMove{
+    private int direction = 1;
 
     public Dragon(Cell cell) {
         super(cell);
@@ -10,6 +11,18 @@ public class Dragon extends Actor implements MonsterMove{
 
     @Override
     public void move() {
+        Cell nextUpperCell = getCell().getNeighbor(0, -1);
+        Cell nextLowerCell = getCell().getNeighbor(0,1);
+        if(!nextUpperCell.getType().isWalkable() || !nextLowerCell.getType().isWalkable()){
+            changeDirection();
+            generalMove(0, direction);
+        } else {
+            generalMove(0, direction);
+        }
+    }
+
+    private void changeDirection () {
+        this.direction = this.direction * -1;
     }
 
     @Override
