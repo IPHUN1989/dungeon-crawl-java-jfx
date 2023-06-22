0127dcd6b8ff4cae9146bf620aa25d3aafd42f15
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-06-22 20:57:21 +0200

    Implement following AI movement

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Ghost.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Ghost.java
@@ -6,7 +6,7 @@ import com.codecool.dungeoncrawl.data.CellType;
 
 import java.util.Random;
 
-public class Ghost extends Actor implements MonsterMove {
+public class Ghost extends Monster implements MonsterMove {
 
     public Ghost(Cell cell, int attack, int health) {
         super(cell, attack, health);
@@ -15,29 +15,17 @@ public class Ghost extends Actor implements MonsterMove {
 
     @Override
     public void move() {
-        Random random = new Random();
-        int[] coordinates = {-1, 0, 1};
-        int coordinate = random.nextInt(2);
-        Cell nextUpperCell = getCell().getNeighbor(0, -1);
-        Cell nextLowerCell = getCell().getNeighbor(0, 1);
-        Cell nextLeftCell = getCell().getNeighbor(-1, 0);
-        Cell nextRightCell = getCell().getNeighbor(1, 0);
-        if (nextUpperCell.hasActor() && nextUpperCell.getActor().getTileName().equals("player")) {
-            generalMove(0, -1);
-            attackOtherActor(getCell(), nextUpperCell);
-        } else if (nextLowerCell.hasActor() && nextLowerCell.getActor().getTileName().equals("player")) {
-            generalMove(0, 1);
-            attackOtherActor(getCell(), nextLowerCell);
-        } else if (nextLeftCell.hasActor() && nextLeftCell.getActor().getTileName().equals("player")) {
-            generalMove(-1, 0);
-            attackOtherActor(getCell(), nextLeftCell);
-        } else if (nextRightCell.hasActor() && nextRightCell.getActor().getTileName().equals("player")) {
-            generalMove(1, 0);
-            attackOtherActor(getCell(), nextRightCell);
-        } else if (coordinate == 0) {
-            super.generalMove(coordinates[random.nextInt(3)], 0);
+        if (playerGetsInSightOfMonster()) {
+            monsterFollowingPlayer();
         } else {
-            super.generalMove(0, coordinates[random.nextInt(3)]);
+            Random random = new Random();
+            int[] coordinates = {-1, 0, 1};
+            int coordinate = random.nextInt(2);
+            if (coordinate == 0) {
+                super.generalMove(coordinates[random.nextInt(3)], 0);
+            } else {
+                super.generalMove(0, coordinates[random.nextInt(3)]);
+            }
         }
     }
 
--- src/main/java/com/codecool/dungeoncrawl/data/actors/Hulk.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Hulk.java
@@ -3,7 +3,10 @@ package com.codecool.dungeoncrawl.data.actors;
 import com.codecool.dungeoncrawl.data.Cell;
 import com.codecool.dungeoncrawl.data.CellType;
 
-public class Hulk extends Actor implements MonsterMove {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hulk extends Monster implements MonsterMove {
 
     public Hulk(Cell cell, int attack, int health) {
         super(cell, attack, health);
@@ -11,22 +14,8 @@ public class Hulk extends Actor implements MonsterMove {
 
     @Override
     public void move() {
-        Cell nextUpperCell = getCell().getNeighbor(0, -1);
-        Cell nextLowerCell = getCell().getNeighbor(0, 1);
-        Cell nextLeftCell = getCell().getNeighbor(-1, 0);
-        Cell nextRightCell = getCell().getNeighbor(1, 0);
-        if (nextUpperCell.hasActor() && nextUpperCell.getActor().getTileName().equals("player")) {
-            generalMove(0, -1);
-            attackOtherActor(getCell(), nextUpperCell);
-        } else if (nextLowerCell.hasActor() && nextLowerCell.getActor().getTileName().equals("player")) {
-            generalMove(0, 1);
-            attackOtherActor(getCell(), nextLowerCell);
-        } else if (nextLeftCell.hasActor() && nextLeftCell.getActor().getTileName().equals("player")) {
-            generalMove(-1, 0);
-            attackOtherActor(getCell(), nextLeftCell);
-        } else if (nextRightCell.hasActor() && nextRightCell.getActor().getTileName().equals("player")) {
-            generalMove(1, 0);
-            attackOtherActor(getCell(), nextRightCell);
+        if (playerGetsInSightOfMonster()) {
+            monsterFollowingPlayer();
         }
     }
 
