9314eb564bae73efb838254ff6cd8f5a0fb728b4
Kristóf Németh <nemethkristof1@gmail.com>
2023-06-22 13:53:10 +0200

    add functionality to render bigger map than the screen

--- src/main/java/com/codecool/dungeoncrawl/ui/UI.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/UI.java
@@ -14,6 +14,8 @@ import javafx.stage.Stage;
 import java.util.Set;
 
 public class UI {
+    private static final double MAX_MAP_WIDTH = 25;
+    private static final double MAX_MAP_HEIGHT = 20;
     private Canvas canvas;
     private GraphicsContext context;
 
@@ -24,8 +26,8 @@ public class UI {
 
     public UI(GameLogic logic, Set<KeyHandler> keyHandlers) {
         this.canvas = new Canvas(
-                logic.getMapWidth() * Tiles.TILE_WIDTH,
-                logic.getMapHeight() * Tiles.TILE_WIDTH);
+                MAX_MAP_WIDTH * Tiles.TILE_WIDTH,
+                MAX_MAP_HEIGHT * Tiles.TILE_WIDTH);
         this.logic = logic;
         this.context = canvas.getGraphicsContext2D();
         this.mainStage = new MainStage(canvas);
@@ -50,19 +52,40 @@ public class UI {
     public void refresh() {
         context.setFill(Color.BLACK);
         context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        for (int x = 0; x < logic.getMapWidth(); x++) {
-            for (int y = 0; y < logic.getMapHeight(); y++) {
-                Cell cell = logic.getCell(x, y);
+
+        for (int i = 0; i < MAX_MAP_WIDTH; i++) {
+            double x = xCoordinateOfFirstCell() + i;
+            for (int j =0; j < MAX_MAP_HEIGHT; j++) {
+                double y = yCoordinateOfFirstCell() + j;
+                Cell cell = logic.getCell((int) x, (int) y);
                 if (cell.getActor() != null) {
-                    Tiles.drawTile(context, cell.getActor(), x, y);
+                    Tiles.drawTile(context, cell.getActor(), i, j);
                 } else if (cell.getItem() != null) {
-                    Tiles.drawTile(context, cell.getItem(), x, y);
+                    Tiles.drawTile(context, cell.getItem(), i, j);
                 } else {
-                    Tiles.drawTile(context, cell, x, y);
+                    Tiles.drawTile(context, cell, i, j);
                 }
             }
         }
         mainStage.setHealthLabelText(logic.getPlayerHealth());
         mainStage.setInventoryLabelText(logic.getInventory());
+    }
+
+    private double xCoordinateOfFirstCell() {
+        int playersXCoordinate = logic.getPlayersXCoordinate();
+        double mapWidth = logic.getMapWidth();
+
+        if (playersXCoordinate < MAX_MAP_WIDTH / 2) return 0;
+        if (playersXCoordinate + MAX_MAP_WIDTH / 2 > mapWidth) return mapWidth - MAX_MAP_WIDTH;
+        return playersXCoordinate - MAX_MAP_WIDTH / 2;
+    }
+
+    private double yCoordinateOfFirstCell() {
+        int playersYCoordinate = logic.getPlayersYCoordinate();
+        double mapHeight = logic.getMapHeight();
+
+        if (playersYCoordinate < MAX_MAP_HEIGHT / 2) return 0;
+        if (playersYCoordinate + MAX_MAP_HEIGHT / 2 > mapHeight) return mapHeight - MAX_MAP_HEIGHT;
+        return playersYCoordinate - MAX_MAP_HEIGHT / 2;
     }
 }
